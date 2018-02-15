dd243b8d1a74bd29541cbdd11412d0a14cae148f
Tomasz Czmyr <tomasz.czmyr@bilansjr.pl>
2018-02-15 19:48:43 +0100

    Vendors

--- src/main/java/guru/springfamework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -2,57 +2,88 @@ package guru.springfamework.bootstrap;
 
 import guru.springfamework.domain.Category;
 import guru.springfamework.domain.Customer;
+import guru.springfamework.domain.Vendor;
 import guru.springfamework.repositories.CategoryRepository;
 import guru.springfamework.repositories.CustomerRepository;
+import guru.springfamework.repositories.VendorRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+/**
+ * Created by jt on 9/24/17.
+ */
 @Component
-public class Bootstrap implements CommandLineRunner {
+public class Bootstrap implements CommandLineRunner{
 
-    CategoryRepository categoryRepository;
-    CustomerRepository customerRepository;
+    private final CategoryRepository categoryRespository;
+    private final CustomerRepository customerRepository;
+    private final VendorRepository vendorRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
-        this.categoryRepository = categoryRepository;
+    public Bootstrap(CategoryRepository categoryRespository, CustomerRepository customerRepository, VendorRepository vendorRepository) {
+        this.categoryRespository = categoryRespository;
         this.customerRepository = customerRepository;
+        this.vendorRepository = vendorRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
         loadCategories();
         loadCustomers();
+        loadVendors();
     }
 
-    private void loadCustomers() {
-        Customer joe = new Customer("Joe", "Newman");
-        Customer michael = new Customer("Michael", "Lachappele");
+    private void loadVendors() {
+        Vendor vendor1 = new Vendor();
+        vendor1.setName("Vendor 1");
+        vendorRepository.save(vendor1);
 
-        customerRepository.save(joe);
-        customerRepository.save(michael);
-
-        System.out.println("Customers loaded " + customerRepository.count());
+        Vendor vendor2 = new Vendor();
+        vendor2.setName("Vendor 2");
+        vendorRepository.save(vendor2);
 
     }
 
     private void loadCategories() {
         Category fruits = new Category();
         fruits.setName("Fruits");
+
         Category dried = new Category();
         dried.setName("Dried");
+
         Category fresh = new Category();
         fresh.setName("Fresh");
+
         Category exotic = new Category();
         exotic.setName("Exotic");
+
         Category nuts = new Category();
         nuts.setName("Nuts");
 
-        categoryRepository.save(fruits);
-        categoryRepository.save(dried);
-        categoryRepository.save(fresh);
-        categoryRepository.save(exotic);
-        categoryRepository.save(nuts);
+        categoryRespository.save(fruits);
+        categoryRespository.save(dried);
+        categoryRespository.save(fresh);
+        categoryRespository.save(exotic);
+        categoryRespository.save(nuts);
 
-        System.out.println("Categories loaded " + categoryRepository.count());
+        System.out.println("Categories Loaded: " + categoryRespository.count());
+    }
+
+    private void loadCustomers() {
+        //given
+        Customer customer1 = new Customer();
+        customer1.setId(1l);
+        customer1.setFirstname("Michale");
+        customer1.setLastname("Weston");
+        customerRepository.save(customer1);
+
+        Customer customer2 = new Customer();
+        customer2.setId(2l);
+        customer2.setFirstname("Sam");
+        customer2.setLastname("Axe");
+
+        customerRepository.save(customer2);
+
+        System.out.println("Customers Loaded: " + customerRepository.count());
     }
 }
